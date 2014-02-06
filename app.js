1c73f7be93428dd9ea9bf9125732d7aa638b567c
anuragpeshne <anurag.peshne@gmail.com>
2014-02-06 09:15:48 +0530

    testing heroku socket config options

--- app.js
+++ app.js
@@ -52,7 +52,9 @@ app.post('/start', startGame.newUser);
 var server =  http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
-io = io.listen(server);
 
+io = io.listen(server);
+io.set('transports', ['xhr-polling']);
+io.set('polling duration', 10);
 startGame.initGame();
 io.sockets.on('connection', startGame.sockOnConnection);
